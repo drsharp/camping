6acf01a3ea2516be3f64a48e0bf3019017b0375a
Dan Sharp <drsharp@sharpdesigns.com>
2013-07-12 01:03:06 -0600

    Added rspec configuration and smoke test rspec for a Trip domain class. Removed unused .gitignore

---
+++ test/spec/domain/trip_spec.rb
@@ -0,0 +1,7 @@
+require 'trip'
+
+describe Trip do
+  it "should be true" do
+    1.should == 1
+  end
+end
---
+++ test/spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# Add all folders and subfolders to load path
+$LOAD_PATH.unshift *Dir.glob("./app/*")
+
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.color_enabled = true
+  config.formatter = :documentation	#:documentation, :progress, :html, :textmate
+
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+
+end
